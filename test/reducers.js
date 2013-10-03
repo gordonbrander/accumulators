8d6e1cb9a562d6cc91f6ac488a4135176149c627
Gordon Brander <marlfoskr@gmail.com>
2013-10-03 10:50:37 -0600

    Add test for map
    Passing

--- test/reducers.js
+++ test/reducers.js
@@ -15,8 +15,8 @@ function log(first) {
   return first;
 }
 
-function assertAsync(value) {
-  return function testAsync(done) {
+function makeAssertK(value) {
+  return function assertK(done) {
     assert(value);
     done();
   }
@@ -26,7 +26,7 @@ describe('Array reduction', function () {
   var a1 = [1, 1, 1];
   var a2 = reduce(a1, sum, 0);
 
-  it('should return immediate results', assertAsync(a2 === 3));
+  it('should return immediate results', makeAssertK(a2 === 3));
 });
 
 describe('Primative reduction', function () {
@@ -34,7 +34,7 @@ describe('Primative reduction', function () {
     return sum * 2;
   });
 
-  it("should call next with value for reduction of primative values.", assertAsync(a3 === 6))
+  it("should call next with value for reduction of primative values.", makeAssertK(a3 === 6))
 });
 
 describe('futureReducible reduction', function () {
@@ -58,11 +58,35 @@ describe('futureReducible reduction', function () {
     return accumulated + num;
   }, 0);
 
-  it('should return a reducible for future values', assertAsync(typeof a.reduce === 'function'));
+  it('should return a reducible for future values', makeAssertK(typeof a.reduce === 'function'));
 
   it('should reduce future values to reduced value', function (done) {
     reduce(a, function (_, sum) {
       assert(sum === 3);
+      done();
+    });
+  });
+
+  it('should not allow multiple reductions on the same source', function(done) {
+    assert.throws(function () {
+      reduce(a, function(_, __) {});
+    }, Error);
+
+    done();
+  });
+});
+
+describe('map', function () {
+  var a = map([0, 0], function () { return 1; });
+
+  it('should return a reducible object', makeAssertK(typeof a.reduce === 'function'));
+
+  it('should transform values per mapping function when reduced', function (done) {
+    var b = reduce(a, sum, 0);
+
+    // ...and a second reduce for the value...
+    reduce(b, function (_, sum) {
+      assert(sum === 2);
       done();
     });
   });
